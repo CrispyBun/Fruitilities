7a43248303c67c9da5d07c44dcfe686a137f7ea0
CrispyBun <74562111+CrispyBun@users.noreply.github.com>
2024-08-28 18:02:36 +0200

    Add shape debug drawing

--- Libraries/cocollision.lua
+++ Libraries/cocollision.lua
@@ -88,4 +88,63 @@ function Shape:setShapeToRectangle(x, y, width, height)
 end
 Shape.setShapeToAABB = Shape.setShapeToRectangle
 
+--------------------------------------------------
+--- ### Shape:debugDraw(fullColor)
+--- Draws the shape for debugging purposes (just visualises the shape's vertices, doesn't reflect the shape's type in any way).  
+--- This is the only platform dependent function. If shape drawing isn't implemented, this function does nothing.
+function Shape:debugDraw(fullColor)
+    return cocollision.graphics.debugDrawShape(self, fullColor)
+end
+
+-- Abstraction for possible usage outside LÖVE -----------------------------------------------------
+-- These are just for visual debugging, and arent't necessary for cocollision to work.
+
+-- Can be replaced with functions to perform these actions in non-love2d environments
+cocollision.graphics = {}
+
+---@diagnostic disable-next-line: undefined-global
+local love = love
+
+local colorMild = {0.25, 0.5, 1, 0.25}
+local colorFull = {0.25, 0.5, 1}
+
+---@param shape Cocollision.Shape
+---@param fullColor boolean
+cocollision.graphics.debugDrawShape = function(shape, fullColor)
+    local color = fullColor and colorFull or colorMild
+
+    local vertices = shape.vertices
+    local verticesFlat = {}
+    for vertexIndex = 1, #vertices do
+        local vertex = vertices[vertexIndex]
+        verticesFlat[#verticesFlat+1] = vertex[1]
+        verticesFlat[#verticesFlat+1] = vertex[2]
+    end
+
+    local cr, cg, cb, ca = love.graphics.getColor()
+
+    if #verticesFlat >= 6 then
+        love.graphics.setColor(color)
+        love.graphics.polygon("fill", verticesFlat)
+    end
+
+    if #verticesFlat >= 4 then
+        love.graphics.setColor(colorFull)
+        love.graphics.line(verticesFlat)
+        love.graphics.line(verticesFlat[#verticesFlat-1], verticesFlat[#verticesFlat], verticesFlat[1], verticesFlat[2])
+    end
+
+    if #verticesFlat >= 2 then
+        love.graphics.setColor(colorFull)
+        love.graphics.points(verticesFlat)
+    end
+
+    love.graphics.setColor(cr, cg, cb, ca)
+end
+
+local emptyFunction = function () end
+if not love then
+    cocollision.graphics.debugDrawShape = emptyFunction
+end
+
 return cocollision
