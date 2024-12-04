fab3a293489c080f8cc634525069fcaaed0dede2
CrispyBun <74562111+CrispyBun@users.noreply.github.com>
2024-12-04 14:14:25 +0100

    Rename query examples

--- Libraries/languava.lua
+++ Libraries/languava.lua
@@ -17,7 +17,7 @@ languava.langs = {}
 ---@field [string] Languava.Language Each language code (e.g. `"en_US"`) mapped to its language definition
 
 ---@class Languava.Language
----@field fields table<string, string> Each text identifier (e.g. `"game.items.sword"`) mapped to its translation (e.g. `"Sword"`)
+---@field fields table<string, string> Each text identifier (e.g. `"game.item.sword"`) mapped to its translation (e.g. `"Sword"`)
 local Language = {}
 local LanguageMT = {__index = Language}
 
@@ -26,7 +26,7 @@ local LanguageMT = {__index = Language}
 
 --- Gets the translation of the given `query`. Will use `languava.currentLanguage` if `langcode` isn't provided.
 --- You can also call this function using `languava()`.
----@param query string The identifier of the translation (e.g. `"game.items.sword"`)
+---@param query string The identifier of the translation (e.g. `"game.item.sword"`)
 ---@param langcode? string
 ---@return string
 function languava.get(query, langcode)
@@ -58,7 +58,7 @@ end
 
 --- Adds a single translation to the specified language.
 ---@param langcode string The language code of the language (e.g. `"en_US"`)
----@param textID string The identifier of the translation (e.g. `"game.items.sword"`)
+---@param textID string The identifier of the translation (e.g. `"game.item.sword"`)
 ---@param translation string The translation itself (e.g. `"Sword"`)
 function languava.addTranslation(langcode, textID, translation)
     local language = languava.langs[langcode] or languava.newLanguage()
